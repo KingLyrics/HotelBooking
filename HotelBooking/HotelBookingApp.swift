d47bdb70e598bc030c47ed6605ce2c5ca44a61e0
aBugInTheSystem <ekom338.com@gmail.com>
2024-06-10 15:41:21 +0300

    Initial Commit

---
+++ HotelBooking/HotelBookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotelBookingApp.swift
+//  HotelBooking
+//
+//  Created by Ekomobong Edeme on 10/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct HotelBookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
